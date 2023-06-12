904f78c4c9f832ea60e7033b465d7fe9b6e348be
Arthur Diniz Meirelles <meirellesarthur@usp.br>
2023-06-12 17:32:19 -0300

    The cltg_view code can now plot more than one function at the same time, and updated .gitignore to ignore .png files.

--- cross-correlation/theoretical/test_plots/cltg_view.py
+++ cross-correlation/theoretical/test_plots/cltg_view.py
@@ -2,21 +2,36 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-direct=input('Cltg_file (with relative or absolute directory location): ')
-df=pd.read_csv(direct, sep=' ', header=None, names=['l', 'cltg'])
-ls, ctg=df['l'], df['cltg']
-Dtg=[ls[i]*(ls[i]+1)*ctg[i]/(2*np.pi) for i in range(len(ls))]
+cont=True
+c=0
 
-#for l in range(1,11):
-#    Dtg[l]=Dtg[l]/(0.3*l)
+fig, ax=plt.subplots()
 
-save=input('Save the image (sa) or just show it (sh)? ')
+while cont:
 
-plt.figure()
-plt.plot(ls[2:], Dtg[2:])
-plt.xlabel(r'$\ell$')
-plt.ylabel(r'$\ell(\ell+1)C_\ell^{tg}/2\pi$')
-plt.xscale('log')
+    direct=input('Cltg_file (with relative or absolute path): ')
+    df=pd.read_csv(direct, sep=' ', header=None, names=['l', 'cltg'])
+    ls, ctg=df['l'], df['cltg']
+    Dtg=[ls[i]*(ls[i]+1)*ctg[i]/(2*np.pi) for i in range(len(ls))]
+
+    lab=input('Label: ')
+    
+    ax.plot(ls[2:], Dtg[2:], label=lab)
+
+    c+=1
+    check=input('Continue plotting? (Y/n) ')
+
+    if check=='n':
+        cont=False
+
+if c>1:
+    ax.legend()
+
+ax.set_xlabel(r'$\ell$')
+ax.set_ylabel(r'$\ell(\ell+1)C_\ell^{tg}/2\pi$')
+ax.set_xscale('log')
+
+save=input('Save (sa) or just show (sh)? ')
 
 if save=='sa':
     file_name=input('Image file name (with image extension, ex: image.png): ')
