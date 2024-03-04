8b30aa74a2358d5d78cf9e9c27d43428d470ab14
Arthur Diniz Meirelles <meirellesarthur@usp.br>
2024-03-04 15:34:02 -0300

    Script for Ctt plot added

---
+++ cross-correlation/theoretical/plotctt.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+ctt_data=pd.read_csv("ctt_LCDM.dat", sep=' ', names=['l', 'cltt'])
+
+#print(ctt_data)
+ls=ctt_data['l']
+Dtt=[ls[i]*(ls[i]+1)/(2*np.pi)*ctt_data['cltt'][i] for i in range(len(ls))]
+
+plt.figure()
+plt.plot(ls[2:], Dtt[2:])
+plt.xscale('log')
+plt.xlabel(r'$\ell$')
+plt.ylabel(r'$\ell(\ell+1)/2\pi C_\ell^{tt}$')
+plt.savefig('ctt_LCDM.png')
