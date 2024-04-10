f0d242a9faccdd1b116fb6c74bedf7e89c3e0dd9
Arthur Diniz Meirelles <10300487@aguia4.hpc.usp.br>
2024-04-10 17:00:10 -0300

    Cluster submition script adapted

--- cross-correlation/theoretical/cobaya/band1_profile.sh
+++ cross-correlation/theoretical/cobaya/band1_profile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --partition=SP2
-#SBATCH --ntasks=10 		# number of tasks / mpi processes
-#SBATCH --cpus-per-task=1 	# Number OpenMP Threads per process
-#SBATCH -J cobaya_test1
+#SBATCH --ntasks=1 		# number of tasks / mpi processes
+#SBATCH --cpus-per-task=9 	# Number OpenMP Threads per process
+#SBATCH -J band1_profile 
 #SBATCH --time=8-00:00:00
 
 #E-mail notifications:
@@ -11,10 +11,10 @@
 #SBATCH --mail-user=meirellesarthur@usp.br
 
 #OpenMP settings:
-export OMP_NUM_THREADS=1
-export MKL_NUM_THREADS=1
-export OMP_PLACES=threads
-export OMP_PROC_BIND=spread
+#export OMP_NUM_THREADS=1
+#export MKL_NUM_THREADS=1
+#export OMP_PLACES=threads
+#export OMP_PROC_BIND=spread
 
 echo $SLURM_JOB_ID		#ID of job allocation
 echo $SLURM_SUBMIT_DIR		#Directory job where was submitted
@@ -24,6 +24,8 @@ echo $SLURM_NTASKS		#Total number of cores for job
 module swap openmpi mpich
 module swap gnu gnu7
 
+source /temporario2/10300487/miniconda3/bin/activate
+
 cd /temporario2/10300487/MscProject/cross-correlation/theoretical/src/
 
 ./compile_lib.sh
@@ -32,4 +34,4 @@ echo "Lib compiled!"
 
 cd /temporario2/10300487/MscProject/cross-correlation/theoretical/cobaya/
 
-python3 profile_OmegaM.py 1 > like_profiles/band1_profile.log 2>&1 #number after script name=2MASS band
+python3 profile_OmegaM.py -n 9 -b 1 > like_profiles/band1_profile_parallel.log 2>&1 #number after script name=2MASS band
