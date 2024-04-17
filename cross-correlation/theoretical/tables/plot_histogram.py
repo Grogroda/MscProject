732f6dbfff029b42abb4e6def4e8c5ff8bec608e
Arthur Diniz Meirelles <meirellesarthur@usp.br>
2024-04-17 15:44:41 -0300

    Histogram plot script now shows gaussian fit alongside data distribution and new figures added to dissertation

--- cross-correlation/theoretical/tables/plot_histogram.py
+++ cross-correlation/theoretical/tables/plot_histogram.py
@@ -3,6 +3,12 @@ from matplotlib import pyplot as plt
 import argparse
 import pandas as pd
 
+def gauss(x, mu, var):
+    k=1/(np.sqrt(2*np.pi*var))
+    exp=np.exp(-1/(2*var)*(x-mu)**2)
+
+    return k*exp
+
 l=2
 
 parser=argparse.ArgumentParser()
@@ -10,7 +16,7 @@ parser.add_argument('-l', '--multipole')
 args=parser.parse_args()
 
 if args.multipole!=None:
-    l=args.multipole
+    l=int(args.multipole)
 print("l=", l)
 
 import matplotlib
@@ -21,9 +27,28 @@ bins, weights=hist[0], hist[1][:-1]
 print(bins)
 print(weights)
 
+#Gauss fit points to plot:
+gauss_fits=pd.read_csv('hists/gauss_fits.txt', header=None, names=['ls', 'mu', 'var'], sep=' ')
+
+xmin, xmax=min(bins), max(bins)
+Npoints=1000
+dx=(xmax-xmin)/Npoints
+xs, ys=[],[]
+x=xmin
+for i in range(Npoints):
+    xs.append(x)
+    mu=gauss_fits['mu'][l]
+    var=gauss_fits['var'][l]
+    y=gauss(x, mu, var)
+    ys.append(y)
+
+    x+=dx
+
 plt.figure(figsize=(10,8))
-plt.hist(bins[:-1], bins, weights=weights)
+plt.hist(bins[:-1], bins, density=True,weights=weights, label='Normalized Distribution')
+plt.plot(xs, ys, label='Gaussian Fit')
 plt.title(r'$\ell={}$'.format(l))
 plt.xlabel(r'$C^{tg}$')
 plt.ylabel(r'$P(C^{tg})$')
+plt.legend(loc='upper left')
 plt.savefig('hist_l{}.png'.format(l))
