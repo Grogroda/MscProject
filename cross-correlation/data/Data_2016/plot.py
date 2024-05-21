385e8c10b9bd86b5a94d988d6496b15e5524dc54
Arthur Diniz Meirelles <meirellesarthur@usp.br>
2024-05-21 19:11:47 -0300

    Theoretical data now shown

--- cross-correlation/data/Data_2016/plot.py
+++ cross-correlation/data/Data_2016/plot.py
@@ -2,25 +2,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-LCDM_correlations=pd.read_csv('../../theoretical/tables/final_table.dat', sep=' ', header=None, names=['ls', 'ctt', 'cgg', 'ctg'])
-ls_theo=LCDM_correlations['ls']
-ctt_theo=LCDM_correlations['ctt']
-cgg_theo=LCDM_correlations['cgg']
-ctg_theo=LCDM_correlations['ctg']
+Ctt_etc=pd.read_csv('../../theoretical/tables/final_table.dat', sep=' ', header=None, names=['ls', 'ctt', 'cgg', 'ctg'])
+ls_ctt=Ctt_etc['ls'][2:]
+ctt_theo=Ctt_etc['ctt'][2:]
 
-Dtt_theo=[ls_theo[i]*(ls_theo[i]+1)*ctt_theo[i]/(2*np.pi) for i in range(len(ls_theo))]
+ls_ctt, ctt_theo=list(ls_ctt), list(ctt_theo)
+
+Dtt_theo=[ls_ctt[i]*(ls_ctt[i]+1)*ctt_theo[i]/(2*np.pi) for i in range(len(ls_ctt))]
 
 #dict={n_band:{col_name:[values], ...}, ...}
 band_dict={}
 arr_names=['ls', 'Dtt', 'err_Dtt', 'Cgg', 'err_Cgg', 'Ctg', 'err_Ctg']
+theo_dict={}
 
 band=1
 while band<=4:
     dict_band=pd.read_csv('errors_data_wmap9QVW_xsc{}_jeffrey_dipfix_50000_ttggtg_new.dat'.format(band), header=None, names=arr_names, sep=' ')
     band_dict[band]=dict_band
+
+    band_corrs=pd.read_csv('../../theoretical/tables/ctg_cgg_band{}.dat'.format(band), sep=' ', header=None, index_col=0, names=['ls', 'ctg', 'cgg'])
+
+    print('len(ctg_theo)=', len(band_corrs['ctg']))
+    theo_dict[band]=band_corrs
+
     band+=1
 
-#print(band_dict)
+print(theo_dict)
 
 import matplotlib
 
@@ -33,32 +40,34 @@ while band<=4:
     print("Starting band {}".format(band))
     #Dtt plot for each band:
     axs[band-1,0].errorbar(band_dict[band]['ls'], band_dict[band]['Dtt'], yerr=band_dict[band]['err_Dtt'], label="Best-fit+total error", fmt='ko')
-    #axs[band-1,0].plot(ls_theo[2:], Dtt_theo[2:], label=r'$\Lamda$CDM fiducial spectrum')
+    axs[band-1,0].plot(ls_ctt, Dtt_theo, label=r'$\Lambda$CDM fiducial spectrum')
     axs[band-1,0].set_ylabel(r'$(\ell+1)\ell/2\pi C_\ell^{tt}$ $[\mu K^2]$', fontsize=20)
     axs[band-1,0].set_xlabel(r'$\ell$', fontsize=20)
     axs[band-1,0].set_xscale('log')
+    axs[band-1,0].set_xlim(1.8, 90)
     axs[band-1,0].set_ylim(0, 4000)
     axs[band-1,0].legend()
     print("Ctt finished")
 
     #Cgg plot for each band:
     axs[band-1,1].errorbar(band_dict[band]['ls'], band_dict[band]['Cgg'], yerr=band_dict[band]['err_Cgg'], fmt='ko')
-    #axs[band-1,1].plot(ls_theo[2:], cgg_theo[2:]) 
+    axs[band-1,1].plot(theo_dict[band]['ls'], theo_dict[band]['cgg']) 
     axs[band-1,1].set_xlabel(r'$\ell$', fontsize=20)
     axs[band-1,1].set_ylabel(r'$C_\ell^{gg}$', fontsize=20)
-    axs[band-1,1].set_ylim(1e-4, 1e-1)
+    axs[band-1,1].set_xlim(1.8, 90)
+    axs[band-1,1].set_ylim(5e-5, 3e-2)
     axs[band-1,1].set_xscale('log')
     axs[band-1,1].set_yscale('log')
     print('Cgg finished')
 
     #Ctg plot for each band:
     axs[band-1,2].errorbar(band_dict[band]['ls'], band_dict[band]['Ctg'], yerr=band_dict[band]['err_Ctg'], fmt='ko')
-    #axs[band-1,2].plot(ls_theo[2:], ctg_theo[2:])
+    axs[band-1,2].plot(theo_dict[band]['ls'], theo_dict[band]['ctg']) 
     axs[band-1,2].set_xlabel(r'$\ell$', fontsize=20)
     axs[band-1,2].set_ylabel(r'$C_\ell^{tg}$ $[\mu K]$', fontsize=20)
-    axs[band-1,2].set_ylim(-0.8, 0.8)
+    axs[band-1,2].set_xlim(1.8, 90)
+    axs[band-1,2].set_ylim(-0.8,0.8)
     axs[band-1,2].set_xscale('log')
-    #axs[band-1,2].set_yscale('log')
     axs[band-1,2].text(32, 0.6, 'Band {}'.format(band))
     print("Band {} finished, moving to next".format(band))
     band+=1
